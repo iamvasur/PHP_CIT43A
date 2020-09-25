ab47289f809eefabbe619aefe4949844ba3642d3
iamvasur <45684566+iamvasur@users.noreply.github.com>
2020-09-25 16:22:11 -0700

    Create helloWorldWAMP.php

---
+++ helloWorldWAMP.php
@@ -0,0 +1,22 @@
+<html>
+    
+    <head>
+        <!-- hello world in PHP -->
+        <title>Hello World!</title>
+    </head>
+
+        <body>
+            <!-- This statement prints hello world in PHP -->
+            <?php 
+
+                $welcome = "Hello World!!! ";
+                $name = "This is Vasu Ranganathan";
+                $greeting = "Welcome to my site";
+                echo '<h1>' . $welcome . $name .'</h1>';
+                echo '<h2>'. $greeting . '</h2>';
+                echo 'This is my third assignment for course CIT 043-A, where the PHP program should output "Hello World" on a web browser using WAMP.' 
+
+            ?> 
+        </body>
+
+</html>
