600a4c4de98d074e4241923781e93a45f982ca82
iamvasur <45684566+iamvasur@users.noreply.github.com>
2020-09-25 14:54:59 -0700

    Create helloWorld.php

---
+++ helloWorld.php
@@ -0,0 +1,8 @@
+<?php 
+
+//Assignment Assignment: Write Hello World with PHP ( Hypertext Preprocessor) scripting language
+
+echo '"Hello World", this is Vasu Ranganathan :-)';
+echo 'This is my second assignment for course CIT 043-A, where the PHP program should output "Hello World".'
+
+?>
